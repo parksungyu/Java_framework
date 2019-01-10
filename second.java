bea0b15d4f4462ad8ed51b91479d868406955a99
parksungyu <46525641+parksungyu@users.noreply.github.com>
2019-01-10 11:50:36 +0900

    Update second.java

--- second.java
+++ second.java
@@ -1,5 +1,22 @@
 package java;
 
+//
+//
+//
+//
+//
+//
+//
+//
+// 1/7일 기술세미나 : java개발도구들
+// 1/8일 기술세미나 : front-end 개발 도구들(HTML, javascript)
+// 1/9일 기술세미나 : jvm개발 도구들
+// www.wikidocs.net/
+// 1/10~12 기술세미나 : (git) 
+// 1) git(git hub)이란 무엇인가 ppt	나만의 git hub를 갖자 		
+// 2)프로파일 git ppt 잘만들어진 git 소개
+// 3) 개인git 생성 - 과제, 수업자료를 업로드
+
 public class second {
 
 	public static void main(String[] args) {
